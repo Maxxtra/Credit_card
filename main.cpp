91e965aad9df4f15e3b3db2e190f3bb9bcc5c43f
Maxxtra <83414814+Maxxtra@users.noreply.github.com>
2021-10-13 19:03:27 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,15 @@
 
 using namespace std;
 
+int initial_validation(string number)
+{
+     if (number == "exit")
+            return 0;
+     else if (!isNumberString(number)) 
+            cout << "typo mistake";
+    return 1;
+}
+
 bool isNumberString(const string& s) {
     int len = s.length();
     int i;
@@ -15,39 +24,33 @@ bool isNumberString(const string& s) {
 
 int main() {
     string number;
-    int i;
 
     while (true) {
 
         cout << "Please enter a CC number to validate: ";
         cin >> number;
 
-        if (ccNumber == "exit")
-            break;
-
-        else if (!isNumberString(number)) {
-            cout << "typo mistake";
-            continue;
-        }
-
-        int len = ccNumber.length();
-        int doubleEvenSum = 0;
+       if(initial_validation(number)==true)
+       {
+           
+        int len = number.length();
+        int even_sum = 0;
+        int i;
 
 
-        for (i = len - 2; i >= 0; i = i - 2) {
-            int dbl = ((ccNumber[i] - 48) * 2);
+        for (i = len - 2; i >= 0; i = i - 2)
+        {
+            int dbl = ((number[i] - 48) * 2);
             if (dbl > 9) {
                 dbl = (dbl / 10) + (dbl % 10);
             }
-            doubleEvenSum += dbl;
+           even_sum += dbl;
         }
 
-
         for (i = len - 1; i >= 0; i = i - 2)
-            doubleEvenSum += (ccNumber[i] - 48);
+            even_sum += (number[i] - 48);
 
-
-        cout << (doubleEvenSum % 10 == 0 ? "Valid!" : "Invalid!") << endl;
+        cout << even_sum % 10 == 0 ? "Valid!\n" : "Invalid!\n";
 
         continue;
     }
